c64af548312eafee48aaeca2a076ba79b3268aec
olivier st-laurent <kzaurckichz@gmail.com>
2022-11-19 17:05:28 -0500

    ...

--- modules/G4D_rays/shaders/1_entity.rchit.glsl
+++ modules/G4D_rays/shaders/1_entity.rchit.glsl
@@ -7,104 +7,104 @@ layout(location = 0) rayPayloadInEXT RayPayload ray;
 
 void main() {
 	
-	// ray.hitDistance = gl_HitTEXT;
-	// ray.id = gl_InstanceCustomIndexEXT;
-	// ray.renderableIndex = gl_InstanceID;
-	// ray.geometryIndex = gl_GeometryIndexEXT;
-	// ray.primitiveIndex = gl_PrimitiveID;
-	// ray.localPosition = gl_ObjectRayOriginEXT + gl_ObjectRayDirectionEXT * gl_HitTEXT;
-	// ray.worldPosition = gl_WorldRayOriginEXT + gl_WorldRayDirectionEXT * gl_HitTEXT;
-	// ray.t2 = 0;
-	// ray.ssao = 0;
+	ray.hitDistance = gl_HitTEXT;
+	ray.id = gl_InstanceCustomIndexEXT;
+	ray.renderableIndex = gl_InstanceID;
+	ray.geometryIndex = gl_GeometryIndexEXT;
+	ray.primitiveIndex = gl_PrimitiveID;
+	ray.localPosition = gl_ObjectRayOriginEXT + gl_ObjectRayDirectionEXT * gl_HitTEXT;
+	ray.worldPosition = gl_WorldRayOriginEXT + gl_WorldRayDirectionEXT * gl_HitTEXT;
+	ray.t2 = 0;
+	ray.ssao = 0;
 	
-	// surface.normal = ComputeSurfaceNormal(ray.localPosition);
-	// surface.color = ComputeSurfaceColor(ray.localPosition);
-	// surface.uv1 = ComputeSurfaceUV1(ray.localPosition);
-	// surface.uv2 = ComputeSurfaceUV2(ray.localPosition);
-	// surface.distance = ray.hitDistance;
-	// surface.localPosition = ray.localPosition;
-	// surface.specular = 0;
-	// surface.emission = vec3(0);
-	// surface.ior = 1.45;
-	// surface.geometryInfo = GEOMETRY.info;
-	// surface.renderableData = INSTANCE.data;
-	// surface.aabbData = 0;
-	// surface.renderableIndex = gl_InstanceID;
-	// surface.geometryIndex = gl_GeometryIndexEXT;
-	// surface.primitiveIndex = gl_PrimitiveID;
-	// surface.aimID = gl_InstanceCustomIndexEXT;
+	surface.normal = ComputeSurfaceNormal(ray.localPosition);
+	surface.color = ComputeSurfaceColor(ray.localPosition);
+	surface.uv1 = ComputeSurfaceUV1(ray.localPosition);
+	surface.uv2 = ComputeSurfaceUV2(ray.localPosition);
+	surface.distance = ray.hitDistance;
+	surface.localPosition = ray.localPosition;
+	surface.specular = 0;
+	surface.emission = vec3(0);
+	surface.ior = 1.45;
+	surface.geometryInfo = GEOMETRY.info;
+	surface.renderableData = INSTANCE.data;
+	surface.aabbData = 0;
+	surface.renderableIndex = gl_InstanceID;
+	surface.geometryIndex = gl_GeometryIndexEXT;
+	surface.primitiveIndex = gl_PrimitiveID;
+	surface.aimID = gl_InstanceCustomIndexEXT;
 	
-	// // if (OPTION_TEXTURES) {
-	// 	// executeCallableEXT(GEOMETRY.info.surfaceIndex, SURFACE_CALLABLE_PAYLOAD);
-	// // }
+	// if (OPTION_TEXTURES) {
+		// executeCallableEXT(GEOMETRY.info.surfaceIndex, SURFACE_CALLABLE_PAYLOAD);
+	// }
 	
-	// vec3 albedo = surface.color.rgb;
+	vec3 albedo = surface.color.rgb;
 	
-	// ray.normal = surface.normal;
+	ray.normal = surface.normal;
 	
-	// // Fresnel
-	// float fresnel = Fresnel((renderer.viewMatrix * vec4(ray.worldPosition, 1)).xyz, normalize(WORLD2VIEWNORMAL * ray.normal), surface.ior);
+	// Fresnel
+	float fresnel = Fresnel((renderer.viewMatrix * vec4(ray.worldPosition, 1)).xyz, normalize(WORLD2VIEWNORMAL * ray.normal), surface.ior);
 	
-	// // Fast Gi Approx
-	// vec3 ambientColor = albedo * renderer.skyLightColor / (4 * 3.1415);
+	// Fast Gi Approx
+	vec3 ambientColor = albedo * renderer.skyLightColor / (4 * 3.1415);
 
-	// // Direct Lighting
-	// vec3 directSunLight = vec3(0);
-	// if (RAY_RECURSIONS < RAY_MAX_RECURSION) {
-	// 	vec3 color = ray.color.rgb;
-	// 	ray.color = vec4(0);
-	// 	vec3 sunDir = normalize(renderer.sunDir);
-	// 	float nDotL = dot(ray.normal, sunDir);
-	// 	if (nDotL > 0) {
-	// 		const vec3 rayOrigin = ray.worldPosition + ray.normal * ray.hitDistance * 0.001;
+	// Direct Lighting
+	vec3 directSunLight = vec3(0);
+	if (RAY_RECURSIONS < RAY_MAX_RECURSION) {
+		vec3 color = ray.color.rgb;
+		ray.color = vec4(0);
+		vec3 sunDir = normalize(renderer.sunDir);
+		float nDotL = dot(ray.normal, sunDir);
+		if (nDotL > 0) {
+			const vec3 rayOrigin = ray.worldPosition + ray.normal * ray.hitDistance * 0.001;
 			
 			
-	// 		// Using Ray Tracing Pipeline (more compatible)
-	// 			RAY_RECURSION_PUSH
-	// 				RAY_SHADOW_PUSH
-	// 					RayPayload originalRay = ray;
-	// 					traceRayEXT(tlas, gl_RayFlagsTerminateOnFirstHitEXT, 0xff/*RENDERABLE_STANDARD_EXCEPT_WATER*/, 0/*rayType*/, 0/*nbRayTypes*/, 0/*missIndex*/, rayOrigin, xenonRendererData.config.zNear, sunDir, xenonRendererData.config.zFar, 0);
-	// 					if (ray.hitDistance == -1) {
-	// 						// lit
-	// 						directSunLight = (albedo * renderer.skyLightColor + ray.color.rgb * fresnel * surface.specular) * nDotL;
-	// 					}
-	// 					ray = originalRay;
-	// 				RAY_SHADOW_POP
-	// 			RAY_RECURSION_POP
+			// Using Ray Tracing Pipeline (more compatible)
+				RAY_RECURSION_PUSH
+					RAY_SHADOW_PUSH
+						RayPayload originalRay = ray;
+						traceRayEXT(tlas, gl_RayFlagsTerminateOnFirstHitEXT, 0xff/*RENDERABLE_STANDARD_EXCEPT_WATER*/, 0/*rayType*/, 0/*nbRayTypes*/, 0/*missIndex*/, rayOrigin, xenonRendererData.config.zNear, sunDir, xenonRendererData.config.zFar, 0);
+						if (ray.hitDistance == -1) {
+							// lit
+							directSunLight = (albedo * renderer.skyLightColor + ray.color.rgb * fresnel * surface.specular) * nDotL;
+						}
+						ray = originalRay;
+					RAY_SHADOW_POP
+				RAY_RECURSION_POP
 			
 			
-	// 		// // Using Ray Query (faster)
-	// 		// 	if (rayQuerySunlight(rayOrigin, sunDir)) {
-	// 		// 		directSunLight = (albedo * renderer.skyLightColor + GetSunColor() * fresnel * surface.specular) * nDotL;
-	// 		// 	}
+			// // Using Ray Query (faster)
+			// 	if (rayQuerySunlight(rayOrigin, sunDir)) {
+			// 		directSunLight = (albedo * renderer.skyLightColor + GetSunColor() * fresnel * surface.specular) * nDotL;
+			// 	}
 			
 			
-	// 		// // Using Ray Query with Soft Shadows
-	// 		// 	int shadowRaySamples = 16;
-	// 		// 	float sunLight = 0;
-	// 		// 	const float sunSolidAngle = 0.05;
-	// 		// 	for (int s = 0; s < shadowRaySamples; ++s) {
-	// 		// 		float pointRadius = sunSolidAngle * RandomFloat(seed);
-	// 		// 		float pointAngle = RandomFloat(seed) * 2.0 * 3.1415926535;
-	// 		// 		vec2 diskPoint = vec2(pointRadius * cos(pointAngle), pointRadius * sin(pointAngle));
-	// 		// 		vec3 lightTangent = normalize(cross(sunDir, ray.normal));
-	// 		// 		vec3 lightBitangent = normalize(cross(lightTangent, sunDir));
-	// 		// 		vec3 shadowRayDir = normalize(sunDir + diskPoint.x * lightTangent + diskPoint.y * lightBitangent);
-	// 		// 		if (rayQuerySunlight(rayOrigin, shadowRayDir)) {
-	// 		// 			++sunLight;
-	// 		// 		}
-	// 		// 	}
-	// 		// 	directSunLight = (albedo * renderer.skyLightColor + GetSunColor() * fresnel * surface.specular) * nDotL * pow(sunLight/shadowRaySamples, 2);
+			// // Using Ray Query with Soft Shadows
+			// 	int shadowRaySamples = 16;
+			// 	float sunLight = 0;
+			// 	const float sunSolidAngle = 0.05;
+			// 	for (int s = 0; s < shadowRaySamples; ++s) {
+			// 		float pointRadius = sunSolidAngle * RandomFloat(seed);
+			// 		float pointAngle = RandomFloat(seed) * 2.0 * 3.1415926535;
+			// 		vec2 diskPoint = vec2(pointRadius * cos(pointAngle), pointRadius * sin(pointAngle));
+			// 		vec3 lightTangent = normalize(cross(sunDir, ray.normal));
+			// 		vec3 lightBitangent = normalize(cross(lightTangent, sunDir));
+			// 		vec3 shadowRayDir = normalize(sunDir + diskPoint.x * lightTangent + diskPoint.y * lightBitangent);
+			// 		if (rayQuerySunlight(rayOrigin, shadowRayDir)) {
+			// 			++sunLight;
+			// 		}
+			// 	}
+			// 	directSunLight = (albedo * renderer.skyLightColor + GetSunColor() * fresnel * surface.specular) * nDotL * pow(sunLight/shadowRaySamples, 2);
 			
 			
-	// 	}
-	// }
+		}
+	}
 	
-	// // Final color
-	// ray.color = vec4(directSunLight + ambientColor, 1);
+	// Final color
+	ray.color = vec4(directSunLight + ambientColor, 1);
 	
-	// // Debug Time
-	// if (xenonRendererData.config.debugViewMode == RENDERER_DEBUG_VIEWMODE_RAYHIT_TIME) {
-	// 	if (RAY_RECURSIONS == 0) WRITE_DEBUG_TIME
-	// }
+	// Debug Time
+	if (xenonRendererData.config.debugViewMode == RENDERER_DEBUG_VIEWMODE_RAYHIT_TIME) {
+		if (RAY_RECURSIONS == 0) WRITE_DEBUG_TIME
+	}
 }
