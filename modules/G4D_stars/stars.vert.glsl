9e188a532959ea4b64df656c47d9f88879de7c38
olivier st-laurent <kzaurckichz@gmail.com>
2022-11-22 10:33:10 -0500

    ...

--- modules/G4D_stars/stars.vert.glsl
+++ modules/G4D_stars/stars.vert.glsl
@@ -15,18 +15,18 @@ void main() {
 	gl_Position = xenonRendererData.config.projectionMatrix * viewMatrix * vec4(pos,1);
 	
 	// Color
-	vec3 color1 = pow(abs(RandomInUnitSphere(seed)) * vec3((1-abs(Simplex(pos/8e11))), abs(Simplex(pos/1e12)), abs(Simplex(pos/2e12))), vec3(0.333));
-	vec3 color2 = pow(abs(RandomInUnitSphere(seed)), vec3(0.333));
+	vec3 color1 = pow(abs(RandomInUnitSphere(seed)) * vec3((1-abs(Simplex(pos/8e11))), abs(Simplex(pos/1e12)), abs(Simplex(pos/2e12))), vec3(0.25));
+	vec3 color2 = pow(abs(RandomInUnitSphere(seed)), vec3(0.25));
 	
 	// Luminosity
 	float smallStars = (abs(Simplex(pos/4e12)) + abs(Simplex(pos/8e12))) * nbRatio;
 	float normalStars = step(0.9, RandomFloat(seed)) * abs(Simplex(pos/8e12)) * nbRatio;
 	float brightStars = step(1 - 5000.0/nbStars, RandomFloat(seed));
-	float veryBrightStars = step(1 - 200.0/nbStars, RandomFloat(seed));
-	float ultraBrightStars = step(1 - 25.0/nbStars, RandomFloat(seed));
+	float veryBrightStars = step(1 - 400.0/nbStars, RandomFloat(seed));
+	float ultraBrightStars = step(1 - 15.0/nbStars, RandomFloat(seed));
 	
 	// Size
-	gl_PointSize = clamp(RandomFloat(seed) * 5 + veryBrightStars + ultraBrightStars, 3, 7);
+	gl_PointSize = clamp(RandomFloat(seed) * 5, 2, 4);
 	// Compensate for darkening on some monitors when moving
 	float starBrightnessCompensationForMonitorDuringMovement = 1.1;
 	if (starBrightnessCompensationForMonitorDuringMovement > 1.001) {
@@ -38,10 +38,10 @@ void main() {
 	
 	// Final color + intensity
 	out_color = vec4(
-		+ color1 * smallStars * 0.2
-		+ color1 * normalStars
+		+ color1 * smallStars * 0.1
+		+ color1 * normalStars * 0.5
 		+ color2 * brightStars * 2
 		+ color2 * veryBrightStars * 64
 		+ color2 * ultraBrightStars * 1024
-	, 1) * 0.5;
+	, 1) * 0.7;
 }
