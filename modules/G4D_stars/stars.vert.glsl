263ac4733a9cca5d8fbd8e7bea088d1d9ac02088
olivier st-laurent <kzaurckichz@gmail.com>
2022-11-22 10:14:54 -0500

    ...

--- modules/G4D_stars/stars.vert.glsl
+++ modules/G4D_stars/stars.vert.glsl
@@ -15,7 +15,7 @@ void main() {
 	gl_Position = xenonRendererData.config.projectionMatrix * viewMatrix * vec4(pos,1);
 	
 	// Color
-	vec3 color1 = pow(abs(RandomInUnitSphere(seed)) * vec3((1-abs(Simplex(pos/1e12))), abs(Simplex(pos/2e12)), abs(Simplex(pos/3e12))), vec3(0.333));
+	vec3 color1 = pow(abs(RandomInUnitSphere(seed)) * vec3((1-abs(Simplex(pos/8e11))), abs(Simplex(pos/1e12)), abs(Simplex(pos/2e12))), vec3(0.333));
 	vec3 color2 = pow(abs(RandomInUnitSphere(seed)), vec3(0.333));
 	
 	// Luminosity
@@ -26,7 +26,15 @@ void main() {
 	float ultraBrightStars = step(1 - 25.0/nbStars, RandomFloat(seed));
 	
 	// Size
-	gl_PointSize = clamp(RandomFloat(seed) * 5 + veryBrightStars + ultraBrightStars, 4, 8);
+	gl_PointSize = clamp(RandomFloat(seed) * 5 + veryBrightStars + ultraBrightStars, 3, 7);
+	// Compensate for darkening on some monitors when moving
+	float starBrightnessCompensationForMonitorDuringMovement = 1.1;
+	if (starBrightnessCompensationForMonitorDuringMovement > 1.001) {
+		vec2 ndc = gl_Position.xy / gl_Position.w;
+		vec2 screenSpacePos = ndc.xy * 0.5 + 0.5;
+		vec2 motion = imageLoad(img_motion, ivec2(screenSpacePos * xenonRendererData.config.renderScale)).xy;
+		if (abs(motion.x) + abs(motion.y) > 0.001) gl_PointSize *= starBrightnessCompensationForMonitorDuringMovement;
+	}
 	
 	// Final color + intensity
 	out_color = vec4(
@@ -35,5 +43,5 @@ void main() {
 		+ color2 * brightStars * 2
 		+ color2 * veryBrightStars * 64
 		+ color2 * ultraBrightStars * 1024
-	, 1) * 0.333;
+	, 1);
 }
