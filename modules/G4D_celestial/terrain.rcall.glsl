b8ff7ac0905afb191dd34c6b365a0efd1f6432e0
olivier st-laurent <kzaurckichz@gmail.com>
2022-12-03 13:19:51 -0500

    ...

--- modules/G4D_celestial/terrain.rcall.glsl
+++ modules/G4D_celestial/terrain.rcall.glsl
@@ -8,15 +8,15 @@ const vec3 sandColor = vec3(224.0/255, 185.0/255, 120.0/255);
 const vec3 rockColor = vec3(184.0/255, 175.0/255, 160.0/255);
 
 float Sand(vec3 pos) {
-	return SimplexFractal(pos*5, 4) + abs(SimplexFractal(pos*0.2, 3)) * 10;
+	return SimplexFractal(pos*5, 6) + abs(SimplexFractal(pos*0.2, 3)) * 10;
 }
 
 float Rock(vec3 pos) {
 	return 
-	 + (clamp(pow(abs(SimplexFractal(pos*2, 5)), 0.8), 0.2, 0.4)) * (clamp(abs(SimplexFractal(pos*2+16.26, 3)), 0.2, 0.5)) * 6
-	 + SimplexFractal(pos*12, 2) * 0.2
-	 - pow(abs(SimplexFractal(pos*2+265.45, 3)), 0.7) * 0.5
-	 - pow(abs(SimplexFractal(pos*2-173.15, 3)), 0.7) * 0.5
+	 + (clamp(pow(abs(SimplexFractal(pos*2, 7)), 0.8), 0.2, 0.4)) * (clamp(abs(SimplexFractal(pos*2+16.26, 4)), 0.2, 0.5)) * 6
+	 + SimplexFractal(pos*12, 4) * 0.2
+	 - pow(abs(SimplexFractal(pos*2+265.45, 4)), 0.7) * 0.5
+	 - pow(abs(SimplexFractal(pos*2-173.15, 4)), 0.7) * 0.5
 	;
 }
 
@@ -35,13 +35,13 @@ float Rock(vec3 pos) {
 void main() {
 	surface.color.rgb = rockColor;
 	if (surface.distance < 500) {
-		float strength = 0.003 * smoothstep(500, 0, surface.distance);
+		float strength = smoothstep(500, 0, surface.distance);
 		float height = Rock(surface.localPosition);
 		if (height > 0.0) {
-			BUMP(Rock, surface.localPosition, surface.normal, strength)
+			BUMP(Rock, surface.localPosition, surface.normal, 0.003 * strength)
 		} else {
 			surface.color.rgb = mix(surface.color.rgb, sandColor, strength);
-			BUMP(Sand, surface.localPosition, surface.normal, strength)
+			BUMP(Sand, surface.localPosition, surface.normal, 0.003 * strength)
 		}
 	}
 }
