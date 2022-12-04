f47dcf08dd88317035b53b820a49f6dba4649a2c
olivier st-laurent <kzaurckichz@gmail.com>
2022-12-03 20:55:09 -0500

    ...

--- modules/G4D_celestial/common.inc.glsl
+++ modules/G4D_celestial/common.inc.glsl
@@ -6,29 +6,24 @@
 #define COMPUTE_SIZE_X 16
 #define COMPUTE_SIZE_Y 16
 
-#define TERRAIN_INT_MULTIPLIER 100
-
 BUFFER_REFERENCE_STRUCT(4) ChunkBuffer {
 	aligned_f64mat4 transform;
 	aligned_f64mat4 inverseTransform;
-	aligned_int32_t baseRadiusInt;
-	aligned_int32_t heightVariationInt;
+	aligned_int64_t baseRadiusMm;
+	aligned_int64_t heightVariationMm;
 	aligned_float32_t skirtOffset;
-	aligned_float32_t triangleSize;
-	aligned_int32_t topSign;
-	aligned_int32_t rightSign;
 };
 
 BUFFER_REFERENCE_STRUCT(4) VertexBuffer {
 	float vertex;
 };
 
-BUFFER_REFERENCE_STRUCT(4) NormalBuffer {
-	float normal;
+BUFFER_REFERENCE_STRUCT(4) UvBuffer {
+	vec2 uv;
 };
 
 PUSH_CONSTANT_STRUCT TerrainChunkPushConstant {
 	BUFFER_REFERENCE_ADDR(ChunkBuffer) chunk;
 	BUFFER_REFERENCE_ADDR(VertexBuffer) vertices;
-	BUFFER_REFERENCE_ADDR(NormalBuffer) normals;
+	BUFFER_REFERENCE_ADDR(UvBuffer) uvs;
 };
--- modules/G4D_celestial/terrain.comp.glsl
+++ modules/G4D_celestial/terrain.comp.glsl
@@ -1,83 +1,63 @@
 #include "common.inc.glsl"
-#include "xenon/renderer/shaders/perlint.glsl"
+#include "xenon/renderer/shaders/GradUint.glsl"
 
 #ifdef GLSL
-	#define PLANET_BASE_RADIUS_INT chunk.baseRadiusInt
-	#define PLANET_HEIGHT_VARIATION_INT chunk.heightVariationInt
+	#define PLANET_BASE_RADIUS_MM chunk.baseRadiusMm
+	#define PLANET_HEIGHT_VARIATION_MM chunk.heightVariationMm
 double TerrainHeightMap(dvec3 normalizedPos)
 #else
-	#define PLANET_BASE_RADIUS_INT (terrainRadius * TERRAIN_INT_MULTIPLIER)
-	#define PLANET_HEIGHT_VARIATION_INT (terrainHeightVariation * TERRAIN_INT_MULTIPLIER)
+	#define PLANET_BASE_RADIUS_MM (terrainRadius * 1000.0)
+	#define PLANET_HEIGHT_VARIATION_MM (terrainHeightVariation * 1000.0)
 static double TerrainHeightMap(const dvec3& normalizedPos, double terrainRadius, double terrainHeightVariation)
 #endif
 /*double TerrainHeightMap(normalizedPos)*/{
-	u32vec3 pos = u32vec3(normalizedPos * PLANET_BASE_RADIUS_INT + 2000000000.0);
+	UVEC3 pos = UVEC3(normalizedPos * PLANET_BASE_RADIUS_MM + 20000000000.0);
 	
-	int32_t baseHeight = int32_t(PLANET_BASE_RADIUS_INT);
+	INT baseHeight = INT(PLANET_BASE_RADIUS_MM);
 	
-	u32vec3 warp = u32vec3(perlint64(pos, 800000, 3000000, 7), perlint64(pos, 800000, 3000000, 7), perlint64(pos, 800000, 3000000, 7));
+	UVEC3 warp = UVEC3(GradUint(pos, 20000000, 20000000, 8), GradUint(pos, 20000000, 20000000, 8), GradUint(pos, 20000000, 20000000, 8));
 	
-	int32_t continents = int32_t(perlint64Ridged(pos + warp*2u, 30000000, 500000, 4) + perlint64Ridged(pos - warp*3u, 280000000, 700000, 3)) - 80000;
-	int32_t bigMountains = int32_t(perlint64Ridged(pos + warp*2u, PLANET_HEIGHT_VARIATION_INT*8, PLANET_HEIGHT_VARIATION_INT/2, 5)) / 4 - 80000;
-	int32_t smallMountains = int32_t(perlint32(pos + warp/10u, 25500, 2000, 3));
+	INT mountains = INT(RidgedGradUint(pos + warp, UINT(PLANET_HEIGHT_VARIATION_MM*8), UINT(PLANET_HEIGHT_VARIATION_MM/2), 24));
+	INT detail = INT(GradUint(pos + warp, 600, 200, 3));
 	
-	int32_t heightInt = baseHeight
-		+ continents
-		+ bigMountains
-		+ smallMountains
+	INT heightMm = baseHeight
+		+ mountains
+		+ detail
 	;
-	return double(heightInt) / double(TERRAIN_INT_MULTIPLIER);
+	return double(heightMm) / 1000.0;
 }
 
 #ifdef GLSL
 	#extension GL_EXT_buffer_reference2 : require
 
 	layout(local_size_x = COMPUTE_SIZE_X, local_size_y = COMPUTE_SIZE_Y) in;
-	
-	vec3 GetVertex(in uint index) {
-		return vec3(vertices[index*3].vertex, vertices[index*3+1].vertex, vertices[index*3+2].vertex);
-	}
-	
-	uint32_t computeSize = gl_NumWorkGroups.x*gl_WorkGroupSize.x;
-	uint32_t vertexSubdivisionsPerChunk = computeSize - 1;
-	uint32_t genCol = gl_GlobalInvocationID.x;
-	uint32_t genRow = gl_GlobalInvocationID.y;
-	uint32_t currentIndex = computeSize * genRow + genCol;
-	uint32_t Xindex = currentIndex*3;
-	uint32_t Yindex = currentIndex*3+1;
-	uint32_t Zindex = currentIndex*3+2;
-	
+
 	void main() {
-		#ifdef SHADER_COMPUTE_TERRAIN_NORMAL
-			// Normal
-			vec3 normal = ComputeNormal();
-			normals[Xindex].normal = normal.x;
-			normals[Yindex].normal = normal.y;
-			normals[Zindex].normal = normal.z;
-		#else
-			// Vertex
-			dvec3 posNorm = normalize((chunk.transform * dvec4(GetVertex(currentIndex), 1)).xyz);
-			dvec3 finalPos = (chunk.inverseTransform * dvec4(posNorm * TerrainHeightMap(posNorm), 1)).xyz;
-			vertices[Xindex].vertex = float(finalPos.x);
-			vertices[Yindex].vertex = float(finalPos.y);
-			vertices[Zindex].vertex = float(finalPos.z);
-			// Skirt
-			int32_t skirtIndex = -1;
-			if (genCol == 0) {
-				skirtIndex = int(genRow);
-			} else if (genCol == vertexSubdivisionsPerChunk) {
-				skirtIndex = int(vertexSubdivisionsPerChunk*4 - vertexSubdivisionsPerChunk - genRow);
-			} else if (genRow == 0) {
-				skirtIndex = int(vertexSubdivisionsPerChunk*4 - genCol);
-			} else if (genRow == vertexSubdivisionsPerChunk) {
-				skirtIndex = int(vertexSubdivisionsPerChunk + genCol);
-			}
-			if (skirtIndex != -1) {
-				vertices[(computeSize*computeSize + skirtIndex) * 3 + 1].vertex = vertices[Yindex].vertex - chunk.skirtOffset;
-				normals[(computeSize*computeSize + skirtIndex) * 3 + 0].normal = 0.0f;
-				normals[(computeSize*computeSize + skirtIndex) * 3 + 1].normal = 1.0f;
-				normals[(computeSize*computeSize + skirtIndex) * 3 + 2].normal = 0.0f;
-			}
-		#endif
+		uint32_t computeSize = gl_NumWorkGroups.x*gl_WorkGroupSize.x;
+		uint32_t vertexSubdivisionsPerChunk = computeSize - 1;
+		uint32_t genCol = gl_GlobalInvocationID.x;
+		uint32_t genRow = gl_GlobalInvocationID.y;
+		uint32_t currentIndex = computeSize * genRow + genCol;
+		uint32_t Xindex = currentIndex*3;
+		uint32_t Yindex = currentIndex*3+1;
+		uint32_t Zindex = currentIndex*3+2;
+		
+		dvec3 posNorm = normalize((chunk.transform * dvec4(vertices[Xindex].vertex, vertices[Yindex].vertex, vertices[Zindex].vertex, 1)).xyz);
+		dvec3 finalPos = (chunk.inverseTransform * dvec4(posNorm * TerrainHeightMap(posNorm), 1)).xyz;
+		vertices[Yindex].vertex = float(finalPos.y);
+		
+		int32_t skirtIndex = -1;
+		if (genCol == 0) {
+			skirtIndex = int(genRow);
+		} else if (genCol == vertexSubdivisionsPerChunk) {
+			skirtIndex = int(vertexSubdivisionsPerChunk*4 - vertexSubdivisionsPerChunk - genRow);
+		} else if (genRow == 0) {
+			skirtIndex = int(vertexSubdivisionsPerChunk*4 - genCol);
+		} else if (genRow == vertexSubdivisionsPerChunk) {
+			skirtIndex = int(vertexSubdivisionsPerChunk + genCol);
+		}
+		if (skirtIndex != -1) {
+			vertices[(computeSize*computeSize + skirtIndex) * 3 + 1].vertex = vertices[Yindex].vertex - chunk.skirtOffset;
+		}
 	}
 #endif
